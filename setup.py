318a3a68bc08a7828fca43874d8e1675d3df14ca
kgangadhar <karthikg1643@gmail.com>
2018-10-23 03:18:15 -0500

    added setup files

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+  name = 'crciapi',
+  packages = ['crciapi'], # this must be the same as the name above
+  version = '1.0',
+  description = 'The cricapi, Free to use, super-high bandwidth, high performance Cricket API. Targeted at Developers and Cricket lovers.',
+  author = 'kgangadhar',
+  author_email = 'karthikg1643@gmail.com',
+  url = 'https://github.com/KarthikGangadhar/pycricapi',
+  download_url = 'https://github.com/KarthikGangadhar/pycricapi/tarball/1.0',
+  keywords = ['cricket',
+    'matchSchedule',
+    'playerstat',
+    'crciketScore',
+    'matches',
+    'fantasysquard',
+    'fantasysummary',
+    'playerfinder'], 
+  classifiers = [],
+)
