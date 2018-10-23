f594fc41f50f4c994dfa85b8070f26d65cc64a0c
kgangadhar <karthikg1643@gmail.com>
2018-10-23 02:56:52 -0500

    refactored code

--- cricapi/Cricapi.py
+++ cricapi/Cricapi.py
@@ -41,10 +41,6 @@ class Cricapi(object):
         data =  r.json()
         return data
 
-# https://cricapi.com/api/fantasySummary?apikey=yiPB2mqlqdNnPa57Vs8P8S74DXk1&unique_id=1034809
-# https://cricapi.com/api/fantasySquad?apikey=yiPB2mqlqdNnPa57Vs8P8S74DXk1&unique_id=1034809
-# https://cricapi.com/api/playerFinder?apikey=yiPB2mqlqdNnPa57Vs8P8S74DXk1&name=Tendulkar
-
     def fantasySummary(self,params):
         """ returns fantasySummary data"""
         URL = self._get_url("fantasySummary",params)
@@ -83,52 +79,3 @@ class Cricapi(object):
             return "{0}/fantasySquad?apikey={1}&unique_id={2}".format(self._url,self._apikey,params["unique_id"])
         elif type == "playerFinder":
             return "{0}/playerFinder?apikey={1}&name={2}".format(self._url,self._apikey,params["name"]) 
-
-
-# https://cricapi.com/api/matches?apikey=yiPB2mqlqdNnPa57Vs8P8S74DXk1
-# https://cricapi.com/api/cricket?apikey=yiPB2mqlqdNnPa57Vs8P8S74DXk1
-# https://cricapi.com/api/cricketScore?apikey=yiPB2mqlqdNnPa57Vs8P8S74DXk1&unique_id=1034809
-# https://cricapi.com/api/matchCalendar?apikey=yiPB2mqlqdNnPa57Vs8P8S74DXk1
-# https://cricapi.com/api/playerStats?apikey=yiPB2mqlqdNnPa57Vs8P8S74DXk1&pid=35320
-# https://cricapi.com/api/fantasySummary?apikey=yiPB2mqlqdNnPa57Vs8P8S74DXk1&unique_id=1034809
-# https://cricapi.com/api/fantasySquad?apikey=yiPB2mqlqdNnPa57Vs8P8S74DXk1&unique_id=1034809
-# https://cricapi.com/api/playerFinder?apikey=yiPB2mqlqdNnPa57Vs8P8S74DXk1&name=Tendulkar
-
-#             
-# # importing the requests library 
-# import requests 
-
-# # api-endpoint 
-# URL = "http://maps.googleapis.com/maps/api/geocode/json"
-
-# # location given here 
-# location = "delhi technological university"
-
-# # defining a params dict for the parameters to be sent to the API 
-# PARAMS = {'address':location} 
-
-# # sending get request and saving the response as response object 
-# r = requests.get(url = URL, params = PARAMS) 
-
-# # extracting data in json format 
-# data = r.json() 
-
-
-# # extracting latitude, longitude and formatted address 
-# # of the first matching location 
-# latitude = data['results'][0]['geometry']['location']['lat'] 
-# longitude = data['results'][0]['geometry']['location']['lng'] 
-# formatted_address = data['results'][0]['formatted_address'] 
-
-# # printing the output 
-# print("Latitude:%s\nLongitude:%s\nFormatted Address:%s"
-# 	%(latitude, longitude,formatted_address)) 
-
-api = Cricapi("yiPB2mqlqdNnPa57Vs8P8S74DXk1")
-# matches = api.matches()
-# cricket = api.cricket()
-# matchCalendar = api.matchCalendar()
-params = {'unique_id':1034809, 'name':'sachin'}
-# params = {'pid':35320}
-cricketScore = api.playerFinder(params)
-print(cricketScore)
