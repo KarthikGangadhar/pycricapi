675bcecf98305f3fdb2d431c19adf23640a9ff13
kgangadhar <karthikg1643@gmail.com>
2018-10-23 03:31:58 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from distutils.core import setup
+
+packages = ['cricapi']
+
+requires = [
+    'requests',
+]
 setup(
   name = 'crciapi',
   packages = ['crciapi'], # this must be the same as the name above
@@ -16,5 +22,21 @@ setup(
     'fantasysquard',
     'fantasysummary',
     'playerfinder'], 
-  classifiers = [],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+
 )
